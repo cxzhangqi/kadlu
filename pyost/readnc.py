5689c97d4169f962427de099d463f8ff6d5e42a8
oliver.kirsebom@dal.ca <oliver.kirsebom@dal.ca>
2018-12-10 16:46:54 -0400

    python script for reading netCDF4 file

--- pyost/readnc.py
+++ pyost/readnc.py
@@ -1,3 +1,14 @@
+
+# The netcdf4-python package is used to read 
+# netCDF files. The packages can be obtained 
+# from:
+#
+#    http://unidata.github.io/netcdf4-python/
+#
+# Note that netcdf4-python has a number of 
+# requirements including the C libraries 
+# HDF5 and netCDF-4. 
+
 from netCDF4 import Dataset
 
 d = Dataset('../res/GEBCO_2014_2D.nc')

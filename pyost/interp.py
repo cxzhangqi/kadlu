1745502dfc479349447bcb9357544a7204527f25
oliver.kirsebom@dal.ca <oliver.kirsebom@dal.ca>
2018-12-21 14:25:44 -0400

    comparison of function call speeds

--- pyost/interp.py
+++ pyost/interp.py
@@ -3,9 +3,11 @@
 # https://stackoverflow.com/questions/31524188/speeding-up-scipys-rectbivariatespline-evaluation-function
 # the ev() function is much slower than direct call to __call__ with grid=True
 
-
 from netCDF4 import Dataset
 from scipy.interpolate import RectBivariateSpline
+import numpy as np
+import time
+
 
 d = Dataset('../res/GEBCO_2014_2D.nc')
 
@@ -19,28 +21,41 @@ z = d.variables['elevation'][int(Nx/2-dN):int(Nx/2+dN),int(Ny/2-dN):int(Ny/2+dN)
 
 bs = RectBivariateSpline(x=x, y=y, z=z)
 
+xi = np.arange(x[0], x[-1], 0.00005)
+yi = np.arange(y[0], y[-1], 0.00005)
 
 
+start = time.time()
+
+xim, yim = np.meshgrid(xi, yi)
+zim = bs.ev(xi=xim, yi=yim)
+
+end = time.time()
+
+print(zim.shape)
+print('ev time: ', end - start)
+
+
+start = time.time()
+
+zi = bs.__call__(x=xi, y=yi, grid=True)  # this method is faster by a factor of 10 !
+
+end = time.time()
+
+print(zi.shape)
+print('call time: ', end - start)
+
+
+
+exit(1)
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.axes3d import Axes3D, get_test_data
 from matplotlib import cm
-import numpy as np
-
 
 fig = plt.figure()
-
 ax = fig.add_subplot(1, 1, 1, projection='3d')
-
-xi = np.arange(x[0], x[-1], 0.002)
-yi = np.arange(y[0], y[-1], 0.002)
-xi, yi = np.meshgrid(xi, yi)
-
-zi = bs.ev(xi=xi, yi=yi)
-
 surf = ax.plot_surface(xi, yi, zi, rstride=1, cstride=1, cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
-#ax.set_zlim(-, 1.01)
 fig.colorbar(surf, shrink=0.5, aspect=10)
-
 plt.show()

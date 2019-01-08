bfd979ed8e9f699fd6fbf8812808861830eb8046
oliver.kirsebom@dal.ca <oliver.kirsebom@dal.ca>
2019-01-08 12:22:25 -0400

    implemented interpolater for lat-lon and unit test

--- pyost/tests/test_bathy_interpolator.py
+++ pyost/tests/test_bathy_interpolator.py
@@ -25,8 +25,40 @@ def test_can_interpolate_bornholm():
     # interpolate without lat-lon constraints
     interp = BathyInterpolator(bathy_reader=reader)
     lat, lon, bathy = reader.read()
-    z = interp.eval_ll(lat=lat[0], lon=lon[0])
-    print(z.shape)
+    # interpolate at single grid point
+    z = interp.eval_ll(lat=lat[0], lon=lon[0]) 
+    z = int(z)
     assert z == bathy[0,0]
+    # interpolate at single point between two grid points
+    x = (lat[1]+lat[2])/2
+    z = interp.eval_ll(lat=x, lon=lon[0]) 
+    z = float(z)
+    zmin = min(bathy[1,0], bathy[2,0])
+    zmax = max(bathy[1,0], bathy[2,0])
+    assert z >= zmin
+    assert z <= zmax
+    # interpolate at two points
+    x1 = (lat[1]+lat[2])/2
+    x2 = (lat[2]+lat[3])/2
+    z = interp.eval_ll(lat=[x1,x2], lon=lon[0]) 
+    zmin = min(bathy[1,0], bathy[2,0])
+    zmax = max(bathy[1,0], bathy[2,0])
+    assert z[0] >= zmin
+    assert z[0] <= zmax
+    zmin = min(bathy[2,0], bathy[3,0])
+    zmax = max(bathy[2,0], bathy[3,0])
+    assert z[1] >= zmin
+    assert z[1] <= zmax
     # interpolate with lat-lon constraints
-    interp = BathyInterpolator(bathy_reader=reader, latlon_SW=LatLon(45.5,4.5), latlon_NE=LatLon(47.5,6.5))
+    interp = BathyInterpolator(bathy_reader=reader, latlon_SW=LatLon(55.10,14.80), latlon_NE=LatLon(55.30,15.10))
+    # interpolate at single grid point
+    m = len(lat)
+    lat = lat[lat >= 55.10]
+    m1 = m - len(lat)
+    n = len(lon)
+    lon = lon[lon >= 14.80]
+    n1 = n - len(lon)
+    bathy = bathy[m1:,n1:]
+    z = interp.eval_ll(lat=lat[0], lon=lon[0]) 
+    z = int(z)
+    assert z == bathy[0,0]

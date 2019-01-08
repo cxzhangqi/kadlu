f200ec70c18f5abb65d5967870085e58758f4eb0
oliver.kirsebom@dal.ca <oliver.kirsebom@dal.ca>
2019-01-08 15:58:22 -0400

    implemented XYtoLL function

--- pyost/tests/test_util.py
+++ pyost/tests/test_util.py
@@ -14,9 +14,9 @@
 import pytest
 import os
 import numpy as np
-from util import LLtoXY
+from util import LLtoXY, XYtoLL
 
-def test_can_convert_single_point():
+def test_can_convert_single_point_from_ll_to_xy():
     lat_ref = 45
     lon_ref = 10
     lats = 45
@@ -30,7 +30,7 @@ def test_can_convert_single_point():
     assert x == pytest.approx(0, 1E3)
     assert y == pytest.approx(111E3, 1E3)
 
-def test_can_convert_several_points():
+def test_can_convert_several_points_from_ll_to_xy():
     lat_ref = 45
     lon_ref = 10
     lats = [45, 45]
@@ -40,3 +40,17 @@ def test_can_convert_several_points():
     assert y[0] == pytest.approx(0, 1E3)
     assert x[1] == 2*x[0]
     assert y[1] == pytest.approx(0, 1E3)
+
+def test_can_convert_single_point_from_xy_to_ll():
+    lat_ref = 45
+    lon_ref = 10
+    xs = 788E3
+    ys = 0
+    lat, lon = XYtoLL(xs, ys, lat_ref, lon_ref)    
+    assert lon == pytest.approx(lon_ref+10, 0.1)
+    assert lat == pytest.approx(lat_ref, 0.1)
+    xs = 0
+    ys = 111E3
+    lat, lon = XYtoLL(xs, ys, lat_ref, lon_ref)    
+    assert lon == pytest.approx(lon_ref, 0.1)
+    assert lat == pytest.approx(lat_ref+1, 0.1)
--- pyost/util.py
+++ pyost/util.py
@@ -1,11 +1,14 @@
 import numpy as np
 
-R1_IUGG = 6371009
 # Equatorial radius (6,378.1370 km)
 # Polar radius (6,356.7523 km)
 # The International Union of Geodesy and Geophysics 
 # (IUGG) defines the mean radius (denoted R1) to be                        
 # 6,371.009 km
+R1_IUGG = 6371009
+
+# Degree to radian conversion factor
+deg2rad = np.pi / 180.
 
 
 def LLtoXY(lat, lon, lat_ref=0, lon_ref=0, rot=0):
@@ -37,18 +40,15 @@ def LLtoXY(lat, lon, lat_ref=0, lon_ref=0, rot=0):
             y: float or numpy array
                 y positions coordinates in meters
     """
-    global R1_IUGG
+    global R1_IUGG, deg2rad
 
     assert np.ndim(lat) == np.ndim(lon), 'lat and lon must have same dimension'
 
-    if np.ndim(lat) == 0:
-        lat = np.array([lat])
-        lon = np.array([lon])
-    else:
-        lat = np.array(lat)
-        lon = np.array(lon)
-
-    deg2rad = np.pi / 180.
+    lat = np.array([lat])
+    lon = np.array([lon])
+    if np.ndim(lat) == 2:
+        lat = np.squeeze(lat)
+        lon = np.squeeze(lon)
 
     R = R1_IUGG
     R2 = R * np.cos(lat_ref * deg2rad)
@@ -57,17 +57,78 @@ def LLtoXY(lat, lon, lat_ref=0, lon_ref=0, rot=0):
     y = (lat - lat_ref) * deg2rad * R
 
     if rot is not 0:
-        SINROT = np.sin(rot * deg2rad)
-        COSROT = np.cos(rot * deg2rad)
-        rotmat = np.array([[COSROT, -SINROT], [SINROT, COSROT]]) 
+        s = np.sin(rot * deg2rad)
+        c = np.cos(rot * deg2rad)
+        rotmat = np.array([[c, -s], [s, c]]) 
         xy = np.array([x, y])
         xy = np.swapaxes(xy, 0,1)
-        XY = rotmat.dot(xy)
-        x = XY[:,0]
-        y = XY[:,1]
+        xy = rotmat.dot(xy)
+        x = xy[:,0]
+        y = xy[:,1]
 
     if len(x) == 1:
         x = float(x)
         y = float(y)
 
     return x, y
+
+def XYtoLL(x, y, lat_ref=0, lon_ref=0, rot=0):
+    """ Transform xy position coordinates to lat-lon coordinates.
+
+        By default, the origin of the xy coordinate system is 
+        set to 0 deg latitude and 0 deg longitude. 
+        
+        By default, the x-axis is aligned with the longitude axis 
+        (west to east) and the y-axis is aligned with the latitude
+        axis (south to north).
+
+        Args: 
+            x: float or numpy array
+                x coordinate of a the location(s) of interest in meters
+            y: float or numpy array
+                y coordinate of a the location(s) of interest in meters
+            lat_ref: float
+                latitude reference coordinate in degrees
+            lon_ref: float
+                longitude reference coordinate in degrees
+            rot: float
+                Rotation angle in degrees for the xy coordinate system 
+                (clockwise rotation).
+
+        Returns:
+            lat: float or numpy array
+                latitude coordinates in degrees
+            lon: float or numpy array
+                longitude coordinates in degrees
+    """
+    global R1_IUGG, deg2rad
+
+    assert np.ndim(x) == np.ndim(y), 'x and y must have same dimension'
+
+    x = np.array([x])
+    y = np.array([y])
+    if np.ndim(x) == 2:
+        x = np.squeeze(x)
+        y = np.squeeze(y)
+
+    R = R1_IUGG
+    R2 = R * np.cos(lat_ref * deg2rad)
+
+    if rot is not 0:
+        s = np.sin(rot * deg2rad)
+        c = np.cos(rot * deg2rad)
+        rotmat = np.array([[c, s], [-s, c]]) 
+        xy = np.array([x, y])
+        xy = np.swapaxes(xy, 0,1)
+        xy = rotmat.dot(xy)
+        x = xy[:,0]
+        y = xy[:,1]
+
+    lon = lon_ref + x / deg2rad / R2
+    lat = lat_ref + y / deg2rad / R
+
+    if len(lat) == 1:
+        lat = float(lat)
+        lon = float(lon)
+
+    return lat, lon

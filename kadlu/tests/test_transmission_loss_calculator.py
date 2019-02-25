def84509ff3fa6009891129798d9b312792d2e1b
oliver.kirsebom@dal.ca <oliver.kirsebom@dal.ca>
2019-02-25 15:20:44 -0400

    started implementing TL calculator

---
+++ kadlu/tests/test_transmission_loss_calculator.py
@@ -0,0 +1,26 @@
+""" Unit tests for the the 'transmission_loss_calculator' module in the 'kadlu' package
+
+    Authors: Oliver Kirsebom
+    contact: oliver.kirsebom@dal.ca
+    Organization: MERIDIAN-Intitute for Big Data Analytics
+    Team: Acoustic data Analytics, Dalhousie University
+    Project: packages/kadlu
+             Project goal: Tools for underwater soundscape modeling
+     
+    License:
+
+"""
+
+import pytest
+import os
+import numpy as np
+from kadlu.transmission_loss_calculator import TransmissionLossCalculator
+
+path_to_assets = os.path.join(os.path.dirname(__file__),"assets")
+
+def test_can_initialize_TL_calculator():
+    c = TransmissionLossCalculator(bathymetry=None, sound_speed=None)
+
+def test_run_TL_calculator():
+    c = TransmissionLossCalculator(bathymetry=None, sound_speed=None)
+    c.run(frequency=10, source_depth=10)
---
+++ kadlu/transmission_loss_calculator.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+class TransmissionLossCalculator():
+
+    def __init__(self, bathymetry, sound_speed):
+
+        self.bathy = bathymetry
+        self.sound_speed = sound_speed
+
+        self.c0 = 1500      # reference sound speed in m/s
+        self.rhow = 1       # water density
+        self.cb = 1700      # homogeneous bottom sound speed
+        self.bloss = 0.5    # homogeneous bottom attenuation db/lambda
+        self.rhob = 1.5     # homogeneous bottom density
+
+        self.nsq = 1 # = (self.c0 / self.sound_speed)^2  refractive index squared
+
+
+    def run(self, frequency, source_depth):
+
+        freq = frequency  # frequency in Hz
+        
+        # source position in meters
+        xs = 0
+        ys = 0
+        zs = source_depth
+
+        lambda0 = self.c0 / freq  # reference wavelength
+
+        # --- set parameters for PE solver ---
+
+        # r
+        steplength = lambda0 / 2         # step size in meters
+        rmax = 50e3                      # radial range in meters
+        ndxout = 1                       # ?
+        numstep = round(rmax/steplength) # number of radial marching steps
+
+        # theta
+        Ltheta = 2 * np.pi # angular domain in radians
+        dtheta = 1/180 * np.pi # angular bin size in radians
+        ntheta = int(np.ceil(Ltheta/dtheta)) # number of angular bins
+        if ntheta%2==1: ntheta = ntheta+1 # ensure even number of angular bins
+        ndy_3DSliceout=1 # ?
+
+        # z
+        dz = 10 # vertical bin size in meters
+        ThinknessOfArtificialAbsorpLayer_ratio_z = 6 # ?
+        nz = 2 * 12e3 / ThinknessOfArtificialAbsorpLayer_ratio_z * (ThinknessOfArtificialAbsorpLayer_ratio_z + 1) / dz # number of vertical bins
+        nz = round(nz/2) * 2  # ensure even number of vertical bins
+        ndz_3DSliceout = 1 # ?
+
+    

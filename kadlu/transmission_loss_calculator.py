47eacc84db9e7085c40b843a0e036bff773d5477
oliver.kirsebom@dal.ca <oliver.kirsebom@dal.ca>
2019-03-05 15:05:58 -0400

    cleaned and documented _create_grids method in TLCalc

--- kadlu/transmission_loss_calculator.py
+++ kadlu/transmission_loss_calculator.py
@@ -28,48 +28,77 @@ class TransmissionLossCalculator():
         ys = 0
         zs = source_depth
 
-        # reference wavelenght and wavenumber
+        # reference wavelength and wavenumber
         lambda0 = self.c0 / freq        
         k0 = 2 * np.pi * freq / self.c0  
 
+        # radial step size
+        dr = lambda0 / 2
+
+        # vertical range
+        ThinknessOfArtificialAbsorpLayer_ratio_z = 6
+        vertical_range = 2 * 12e3 / ThinknessOfArtificialAbsorpLayer_ratio_z * (ThinknessOfArtificialAbsorpLayer_ratio_z + 1)
+
         # create grids
-        Z = self._create_grids(lambda0)
+        Z = self._create_grids(radial_step=dr, radial_range=50e3,\
+                azimuthal_step=1/180*np.pi, azimuthal_range=2*np.pi,\
+                vertical_step=10, vertical_range=vertical_range)
 
         # PE starter
         starter = PEStarter(method='THOMSON', aperture=88)
 
 
-    def _create_grids(self, lambda0):
+    def _create_grids(self, radial_step, radial_range,\
+            azimuthal_step, azimuthal_range, vertical_step, vertical_range):
+        """ Create grids for PE solver
+            
+            Args:
+                radial_step: float
+                    Radial step size (in meters)
+                radial_range: float
+                    Radial range (in meters)
+                azimuthal_step: float
+                    Angular step size (in radians)
+                azimuthal_range: float
+                    Angular domain (in radians)
+                vertical_step: float
+                    Vertical step size (in meters)
+                vertical_range: float
+                    Vertical range (in meters)
 
-        # ------ r (x) ------ 
-        steplength = lambda0 / 2         # step size in meters
-        rmax = 50e3                      # radial range in meters
-        numstep = round(rmax / steplength) # number of radial marching steps
-        dx = steplength
-        x = np.arange(numstep+1, dtype=float)
+            Returns:
+
+        """
+        dx = radial_step
+        xmax = radial_range
+        dy = azimuthal_step
+        ymax = azimuthal_range
+        dz = vertical_step
+        zmax = vertical_range
+
+        # ------ x (radial) ------ 
+        nx = round(xmax / dx) # number of radial bins
+        x = np.arange(nx+1, dtype=float)
         x *= dx
 
-        # ------ theta (y) ------ 
-        Ltheta = 2 * np.pi # angular domain in radians
-        dtheta = 1/180 * np.pi # angular bin size in radians
-        ntheta = int(np.ceil(Ltheta/dtheta)) # number of angular bins
-        if ntheta%2==1: ntheta = ntheta+1 # ensure even number of angular bins
-        dy = dtheta
-        ny = ntheta
-        Ly = dy * ny   # rough estimate of angular aperature
+        # ------ y (azimuthal) ------ 
+        ny = int(np.ceil(ymax / dy)) # number of angular bins
+        if ny%2==1: ny = ny + 1 # ensure even number of angular bins
         y_pos = np.arange(ny/2, dtype=float)
         y_neg = np.arange(-ny/2-1, step=-1, dtype=float)
-        y = np.concatenate((y_pos, y_neg)) * dy
+        y = np.concatenate((y_pos, y_neg)) 
+        y *= dy
 
-        # ------ z ------ 
-        dz = 10 # vertical bin size in meters
-        ThinknessOfArtificialAbsorpLayer_ratio_z = 6 # ?
-        nz = 2 * 12e3 / ThinknessOfArtificialAbsorpLayer_ratio_z * (ThinknessOfArtificialAbsorpLayer_ratio_z + 1) / dz # number of vertical bins
-        nz = round(nz/2) * 2  # ensure even number of vertical bins
+        # ------ z (vertical) ------ 
+        nz = zmax / dz # number of vertical bins
+        nz = round(nz / 2) * 2  # ensure even number of vertical bins
         Lz = nz * dz  
         z_pos = np.arange(nz/2, dtype=float)
         z_neg = np.arange(-nz/2-1, step=-1, dtype=float)
-        z = np.concatenate((z_pos, z_neg)) * dz
+        z = np.concatenate((z_pos, z_neg))
+        z *= dz
+
+        # ------ kz (wavenumber) ------
         kz = z * 2 * np.pi / (Lz * dz)
 
 

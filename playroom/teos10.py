b9f678d805fe839dbe569ef2ed2ef758befbcddd
oliver.kirsebom@dal.ca <oliver.kirsebom@dal.ca>
2019-07-25 11:49:15 -0300

    added conda-forge channel to env file

--- playroom/teos10.py
+++ playroom/teos10.py
@@ -1,4 +1,5 @@
 import numpy as np
+import gsw
 from gsw.density import sound_speed
 
 # scalars
@@ -18,3 +19,11 @@ CT = np.array([[1, 2],[3, 4]])
 p = np.array([[1, 2],[3, 4]])
 c = sound_speed(SA=SA, CT=CT, p=p)
 print(c)
+
+
+#https://teos-10.github.io/GSW-Python/gsw_flat.html#gsw.p_from_z
+
+p = gsw.p_from_z(z=1000, lat=0)
+print('p at +1000 m:  {0} dbar'.format(p))
+p = gsw.p_from_z(z=-1000, lat=0)
+print('p at -1000 m:  {0} dbar'.format(p))

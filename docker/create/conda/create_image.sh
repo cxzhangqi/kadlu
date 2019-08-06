10ab274dd627074fac0eaa48b61897c9472be6fb
oliver.kirsebom@dal.ca <oliver.kirsebom@dal.ca>
2019-08-06 14:42:48 -0300

    fixed docker image

--- docker/create/conda/create_image.sh
+++ docker/create/conda/create_image.sh
@@ -6,13 +6,13 @@ cp ../../../environment.yml .
 cp ../../../../meridian-rtd-theme/dist/sphinx_mer_rtd_theme-0.4.3.dev0.tar.gz .
 
 # build image
-docker build --tag=kadlu_v0.0.3 .
+docker build --tag=kadlu_v0.0.1 .
 
 # tag image
-docker tag kadlu_v0.0.3 oliskir/kadlu:v0.0.3
+docker tag kadlu_v0.0.1 oliskir/kadlu:v0.0.1
 
 # push image to repository
-docker push oliskir/kadlu:v0.0.3
+docker push oliskir/kadlu:v0.0.1
 
 # clean
 rm -rf kadlu

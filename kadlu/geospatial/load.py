79e4ed5fd36badd7c8fbd73e525525e30d5f3009
Oliver Kirsebom <oliver.kirsebom@gmail.com>
2019-08-16 16:42:16 -0300

    just method instead of class

--- kadlu/geospatial/load.py
+++ kadlu/geospatial/load.py
@@ -15,55 +15,46 @@ from kadlu.geospatial.fetch import fetch_bathy_chs
 from kadlu.geospatial.bathy_reader import LatLon
 
 
-class BathyCHSLoader():
-    """ Class for loading Canadian Hydrographic Service Non-Navigational (NONNA-100) Bathymetric Data.
-    
-        Args:
-            path: str
-                Full path to the directory where the bathymetry data files are stored.
+def load(latlon_SW=LatLon(-90,-180), latlon_NE=LatLon(90,180)):
+    """ Load available bathymetry data within specified geographical region.
+
+        Args: 
+            latlon_SW: LatLon
+                South-western (SW) boundary of the region of interest.
+            latlon_NE: LatLon
+                North-eastern (SE) boundary of the region of interest.
+
+        Returns:
+            lat: 1d numpy array
+                Latitude values
+            lon: 1d numpy array
+                Longitude values
+            bathy: 1d numpy array
+                Bathymetry values
     """
-    def __init__(self, path):
+    # fetch data
+    files = fetch_bathy_chs(latlon_SW, latlon_NE)
 
-    def load(self, latlon_SW=LatLon(-90,-180), latlon_NE=LatLon(90,180)):
-        """ Load available bathymetry data within specified geographical region.
+    bathy, lat, lon = list(), list(), list()        
 
-            Args: 
-                latlon_SW: LatLon
-                    South-western (SW) boundary of the region of interest.
-                latlon_NE: LatLon
-                    North-eastern (SE) boundary of the region of interest.
+    # loop over geotiff files
+    for f in files:
 
-            Returns:
-                lat: 1d numpy array
-                    Latitude values
-                lon: 1d numpy array
-                    Longitude values
-                bathy: 1d numpy array
-                    Bathymetry values
-        """
-        # fetch data
-        files = fetch_bathy_chs(latlon_SW, latlon_NE)
+        # read data from geotiff file
+        v, _, _ = read_geotiff_2d(path=f)
 
-        bathy, lat, lon = list(), list(), list()        
+        # create lat,lon arrays
+        x = np.ones(1)
+        y = np.ones(1)
 
-        # loop over geotiff files
-        for f in files:
-
-            # read data from geotiff file
-            v, _, _ = read_geotiff_2d(path=f)
-
-            # create lat,lon arrays
-            x = np.ones(1)
-            y = np.ones(1)
-
-            lat.append(x)
-            lon.append(y)
-            bathy.append(v)
+        lat.append(x)
+        lon.append(y)
+        bathy.append(v)
 
 
-        # concatenate
-        bathy = np.concatenate(bathy)
-        lat = np.concatenate(lat)
-        lon = np.concatenate(lon)
+    # concatenate
+    bathy = np.concatenate(bathy)
+    lat = np.concatenate(lat)
+    lon = np.concatenate(lon)
 
-        return bathy, lat, lon
+    return bathy, lat, lon

47cba613d552628bf9275b0a5d76d99b527ef93d
oliver.kirsebom@dal.ca <oliver.kirsebom@dal.ca>
2019-08-20 10:11:22 -0300

    fixed failing test

--- kadlu/tests/geospatial/data_sources/test_gebco.py
+++ kadlu/tests/geospatial/data_sources/test_gebco.py
@@ -13,14 +13,13 @@
 import pytest
 import os
 import numpy as np
-import kadlu.geospatial.load as geo
-from kadlu.geospatial.bathy_reader import LatLon
+import kadlu.geospatial.data_sources.gebco as geb
 
 path_to_assets = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "assets")
 
 def test_load_bathymetry_for_bornholm():
     path = os.path.join(path_to_assets, "bornholm.nc")
-    bathy, lats, lons = geo.load_bathy(storage_location=path, source="GEBCO")
+    bathy, lats, lons = geb.load(storage_location=path)
     assert np.min(bathy) == -100
     assert np.max(bathy) == 159
     assert bathy.shape[0] == lats.shape[0]
@@ -28,8 +27,8 @@ def test_load_bathymetry_for_bornholm():
 
 def test_load_bathymetry_for_parts_of_bornholm():
     path = os.path.join(path_to_assets, "bornholm.nc")
-    bathy1, lats1, lons1 = geo.load_bathy(storage_location=path, west=14.5, east=14.9, source="GEBCO")
-    bathy2, lats2, lons2 = geo.load_bathy(storage_location=path, west=14.9, east=15.5, source="GEBCO")
+    bathy1, lats1, lons1 = geb.load(storage_location=path, west=14.5, east=14.9)
+    bathy2, lats2, lons2 = geb.load(storage_location=path, west=14.9, east=15.5)
     assert min(np.min(bathy1), np.min(bathy2)) == -100
     assert max(np.min(bathy1), np.min(bathy2)) > -100
     assert max(np.max(bathy1), np.max(bathy2)) == 159

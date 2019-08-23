881719310529bb537e678462cc57c1db1aaba5a1
matt_S <matthew.smith@dal.ca>
2019-08-23 15:31:35 +0000

    experimenting with projection conversions

---
+++ playroom/projections.py
@@ -0,0 +1,22 @@
+import numpy as np
+from pyproj import Proj, transform
+from kadlu.geospatial.data_sources import chs
+from kadlu.geospatial.data_sources import fetch_util
+from osgeo import gdal
+
+# convert from WGS 84 (global) to spherical mercator
+inproj = Proj(init="epsg:4326")
+outproj = Proj(init="epsg:3857")
+
+# fetch CHS data, filter out .tfw files, grab the first .tif
+fpaths = chs.fetch(south=45, west=-84, north=47, east=-81)
+mask = ["tfw" not in x for x in fpaths]
+tifs = np.array(fpaths)[mask]
+bathy = gdal.Open(tifs[0])
+
+# read values and convert
+# no idea what z1 and z2 are, looks like all zeroes ??
+x1, x1binsize, z1, y1, z2, y1binsize = bathy.GetGeoTransform()
+
+x2, y2 = transform(inproj, outproj, x1, y1)
+print(x2, y2)

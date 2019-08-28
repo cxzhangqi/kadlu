1b167d4d00c16e404007525e3b61fd3835f642a5
matt_S <matthew.smith@dal.ca>
2019-08-28 19:58:27 +0000

    hycom fetching now converts lat/lon input to grid index. TODO: fetch temp

--- kadlu/geospatial/data_sources/hycom.py
+++ kadlu/geospatial/data_sources/hycom.py
@@ -1,17 +1,26 @@
 import numpy as np
 import requests
+from kadlu.geospatial.data_sources import fetch_util
 
-def fetch():
-    # in the future there will be a function to abstract time, depth, lats, 
-    # and lons based on inputs to this function 
-    # matt_s 2019-08
+
+def fetch(south=-90, north=90, west=-180, east=180):
+    """
+        Fetches salinity for the given area.
+
+        TODO:
+            allow time range input
+            allow depth input ???
+            fetch temperature
+    """
     year = "2015"
     source = f"https://tds.hycom.org/thredds/dodsC/GLBv0.08/expt_53.X/data/{year}.ascii?"
 
+    lat, lon = load_grid()
+
     t = (0, 2)      # time
     d = (0, 3)      # depth
-    x = (900, 940)  # lat
-    y = (800, 830)  # lon
+    x = (ll_2_xy(west, lon),    ll_2_xy(east, lon))
+    y = (ll_2_xy(south, lat),   ll_2_xy(north, lat))
 
     slx = lambda tup, step=1: f"[{tup[0]}:{step}:{tup[1]}]"
     varslices = lambda var, slxs : f"{var}{''.join([slx(v) for v in slxs])}"
@@ -35,5 +44,34 @@ def fetch():
     #    header, vals = arr.split("\n")
     #    print(f"{header}:\t{vals}")
 
-    return salinity
+    return salinity, lat[y[0]:y[1]], lon[x[0]:x[1]]
+
+def fetch_grid():
+    print("Fetching latitude and longitude grid from HYCOM...")
+    url = "https://tds.hycom.org/thredds/dodsC/GLBv0.08/expt_53.X/data/2015.ascii?lat%5B0:1:3250%5D,lon%5B0:1:4499%5D"
+    grid_txt = requests.get(url)
+    assert(grid_txt.status_code == 200)
+
+    meta, data = grid_txt.text.split("---------------------------------------------\n")
+    lat_csv, lon_csv = data.split("\n\n")[:-1]
+    lat = np.array(lat_csv.split("\n")[1].split(", "), dtype=np.float)
+    lon = np.array(lon_csv.split("\n")[1].split(", "), dtype=np.float)
+
+    stor_loc = fetch_util.instantiate_storage_config()
+    np.save(f"{stor_loc}hycom_lats.npy", lat, allow_pickle=False)
+    np.save(f"{stor_loc}hycom_lons.npy", lon, allow_pickle=False)
+
+def load_grid():
+    storage = fetch_util.instantiate_storage_config()
+    def loadLatLon(storage): return np.load(f"{storage}hycom_lats.npy"), np.load(f"{storage}hycom_lons.npy")
+    try:
+        return loadLatLon(storage)
+    except FileNotFoundError:
+        fetch_grid()
+        return loadLatLon(storage)
+
+def ll_2_xy(key, array): 
+    """ converts lat/lon values to grid index """
+    if key > array[-1]: return len(array) - 1
+    return np.nonzero(array >= key)[0][0]
 

b926c965fd5ca3c14d38a623f2140e1a9c27bca7
matt_S <matthew.smith@dal.ca>
2019-08-30 16:44:37 +0000

    era5 fetching (wave height, direction, period) passing tests. TODO: write era5 load tests

--- kadlu/geospatial/data_sources/era5.py
+++ kadlu/geospatial/data_sources/era5.py
@@ -1,21 +1,28 @@
+"""
+    API for Era5 dataset from Copernicus Climate Datastore
+
+    Metadata regarding the dataset can be found here:
+        https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-single-levels?tab=overview
+
+    matt_s 2019-08
+"""
 import numpy as np
 from datetime import datetime, timedelta
 import os
 import cdsapi
 import pygrib
-
+from kadlu.geospatial.data_sources import fetch_util 
 from kadlu.geospatial.data_sources.fetch_util import storage_cfg 
 
-def fetchname(wavevar, time)
+def fetchname(wavevar, time):
     return f"ERA5_reanalysis_{wavevar}_{time.strftime('%Y-%m-%d_%Hh')}.grb2"
 
 def fetch_era5(wavevar, time):
     fetchfile = f"{storage_cfg()}{fetchname(wavevar, time)}"
-
     if os.path.isfile(fetchfile):
-        print(f"File {fname} exists, skipping retrieval...")
+        print(f"File {fetchname(wavevar, time)} exists, skipping retrieval...")
     else:
-        print(f"Downloading {wavevar} from Copernicus Climate Data Store...")
+        print(f"Downloading {fetchname(wavevar, time)} from Copernicus Climate Data Store...")
         c = cdsapi.Client()
         c.retrieve('reanalysis-era5-single-levels', {
             'product_type'  : 'reanalysis',
@@ -26,17 +33,33 @@ def fetch_era5(wavevar, time):
             'day'           : time.strftime("%d"),
             'time'          : time.strftime("%H:%M")
         }, fetchfile)
-
     return fetchfile
 
-def fetch_waveheight(time=datetime.now()):      return fetch_era5('significant_height_of_combined_wind_waves_and_swell', time)
-def fetch_wavedirection(time=datetime.now()):   return fetch_era5('mean_wave_direction', time)
-def fetch_waveperiod(time=datetime.now()):      return fetch_era5('mean_wave_period', time)
+class Era5():
+    def fetch_waveheight(self, time=datetime.now()):      
+        return fetch_era5('significant_height_of_combined_wind_waves_and_swell', time)
+    
+    def fetch_wavedirection(self, time=datetime.now()):   
+        return fetch_era5('mean_wave_direction', time)
 
-fetch = [fetch_waveheight, fetch_wavedirection, fetch_waveperiod]
+    def fetch_waveperiod(self, time=datetime.now()):      
+        return fetch_era5('mean_wave_period', time)
+
+    def load(self, filepath, plot=False): return fetch_util.loadgrib(filepath, plot)
+
+    header = "(time=datetime.now())"
+
+    def printFcns(self): 
+        for fcn in [self.fetch_waveheight, self.fetch_wavedirection, self.fetch_waveperiod]:
+            print(fcn.__name__ + self.header)
+
+"""
+Era5().printFcns()
+"""
 
 
-def load(filepath, plot=False): return fetch_util.loadgrib(filepath, plot)
+
+
 
 
 """
--- kadlu/tests/geospatial/data_sources/test_era5.py
+++ kadlu/tests/geospatial/data_sources/test_era5.py
@@ -1,33 +1,32 @@
 import pytest
 from datetime import datetime, timedelta
-from kadlu.geospatial.data_sources import era5
-from kadlu.geospatial.data_sources import fetch_util
+#from kadlu.geospatial.data_sources import era5
+from kadlu.geospatial.data_sources.era5 import Era5
+from kadlu.geospatial.data_sources.fetch_util import storage_cfg
 
-def test_fetch_era5_waveheight():
-    era5.fetch_waveheight(time=datetime(2018, 1, 1, 0, 0, 0, 0))
+# time used for testing
+time = datetime(2018, 1, 1, 0, 0, 0, 0)
 
-def test_fetch_era5_wavedirection():
-    era5.fetch_wavedirection(time=datetime(2018, 1, 1, 0, 0, 0, 0))
+def test_era5_fetch_waveheight():
+    Era5().fetch_waveheight(time)
 
-def test_fetch_era5_waveperiod():
-    era5.fetch_waveperiod(time=datetime(2018, 1, 1, 0, 0, 0, 0))
+def test_era5_fetch_wavedirection():
+    Era5().fetch_wavedirection(time)
+
+def test_era5_fetch_waveperiod():
+    Era5().fetch_waveperiod(time)
+
+def test_era5_load_waveheight():
+    pass
+
+def test_era5_load_wavedirection():
+    pass
+
+def test_era5_load_waveperiod():
+    pass
 
 def test_plot_era5_waveheight():
-    storage_location = fetch_util.instantiate_storage_config()
     filename = "ERA5_reanalysis_significant_height_of_combined_wind_waves_and_swell_2018-01-01_00h.grb2"
-    filepath = f"{storage_location}{filename}"
-    data = era5.load(filepath=filepath, plot=True)
-
-"""
-mahone bay test area:
-north =  44.7
-south =  44.4
-west  = -64.4
-east  = -63.8
-"""
-
-def test_fetch_era5_fetchall():
-    # for fcn in era5.fetch:
-    #     datafiles = fcn(time=datetime(2018, 1, 1, 0, 0, 0, 0))
-    datafiles = map(lambda fcn : fcn(time=datetime(2018, 1, 1, 0, 0, 0, 0)), era5.fetch)
+    filepath = f"{storage_cfg()}{filename}"
+    data = Era5().load(filepath=filepath, plot="Wave Height 2018-01-01")
 

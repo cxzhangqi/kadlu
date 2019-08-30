504fa83bca87c054aa3d78de66a07e0f1d7890ea
matt_S <matthew.smith@dal.ca>
2019-08-30 16:53:49 +0000

    cleaning up era5 module

--- kadlu/geospatial/data_sources/era5.py
+++ kadlu/geospatial/data_sources/era5.py
@@ -17,6 +17,7 @@ from kadlu.geospatial.data_sources.fetch_util import storage_cfg
 def fetchname(wavevar, time):
     return f"ERA5_reanalysis_{wavevar}_{time.strftime('%Y-%m-%d_%Hh')}.grb2"
 
+
 def fetch_era5(wavevar, time):
     fetchfile = f"{storage_cfg()}{fetchname(wavevar, time)}"
     if os.path.isfile(fetchfile):
@@ -35,8 +36,9 @@ def fetch_era5(wavevar, time):
         }, fetchfile)
     return fetchfile
 
+
 class Era5():
-    def fetch_waveheight(self, time=datetime.now()):      
+    def fetch_windwaveswellheight(self, time=datetime.now()):      
         return fetch_era5('significant_height_of_combined_wind_waves_and_swell', time)
     
     def fetch_wavedirection(self, time=datetime.now()):   
@@ -45,52 +47,16 @@ class Era5():
     def fetch_waveperiod(self, time=datetime.now()):      
         return fetch_era5('mean_wave_period', time)
 
-    def load(self, filepath, plot=False): return fetch_util.loadgrib(filepath, plot)
+    def load(self, filepath, plot=False): 
+        """
+        grbs=pygrib.open(grib)
+        grb = grbs.select(validDate=target_date,shortNameECMF=wavevar.value)[0]
+        """
+        return fetch_util.loadgrib(filepath, plot)
 
     header = "(time=datetime.now())"
 
-    def printFcns(self): 
+    def print_fcns(self): 
         for fcn in [self.fetch_waveheight, self.fetch_wavedirection, self.fetch_waveperiod]:
             print(fcn.__name__ + self.header)
-
-"""
-Era5().printFcns()
-"""
-
-
-
-
-
-
-"""
-    # If no gribfile argument is provided, default to the fetched file.
-    if grib is None:
-        grib = self.fetch_filename
-
-    # load grib structure from target.
-    grbs=pygrib.open(grib)
-
-    # Identify parameter and date for extraction.
-    # Date field: validDate
-    # Target date (not incl. time yet)
-    if (target_date is None):
-        target_date = self.fetch_datetimestamp.replace(minute=0, hour=0, second=0, microsecond=0)
-    else:
-### Should any filtering on time be added here to enforce valid time intervals?
-        target_date = target_date
-
-    # Fetch the indicated slice from the overall Grib file.
-    grb = grbs.select(validDate=target_date,shortNameECMF=wavevar.value)[0]
-
-    if(wavevar == ERA5Wavevar.significant_height_of_combined_wind_waves_and_swell):
-        title_text = "ERA5 Sig. Wave + Swell Height from GRIB\n({}) ".format(target_date)
-    elif(wavevar == ERA5Wavevar.mean_wave_period):
-        title_text = "ERA5 Mean Wave Period from GRIB\n({}) ".format(target_date)
-    elif(wavevar == ERA5Wavevar.mean_wave_direction):
-        title_text = "ERA5 Mean Wave Direction from GRIB\n({}) ".format(target_date)
-    else:
-        title_text = "ERA5\nUnknown variable from GRIB\n({}) ".format(target_date)
-
-    return (grb, title_text)
-"""
 
--- kadlu/tests/geospatial/data_sources/test_era5.py
+++ kadlu/tests/geospatial/data_sources/test_era5.py
@@ -7,8 +7,8 @@ from kadlu.geospatial.data_sources.fetch_util import storage_cfg
 # time used for testing
 time = datetime(2018, 1, 1, 0, 0, 0, 0)
 
-def test_era5_fetch_waveheight():
-    Era5().fetch_waveheight(time)
+def test_era5_fetch_windwaveswellheight():
+    Era5().fetch_windwaveswellheight(time)
 
 def test_era5_fetch_wavedirection():
     Era5().fetch_wavedirection(time)
@@ -16,7 +16,7 @@ def test_era5_fetch_wavedirection():
 def test_era5_fetch_waveperiod():
     Era5().fetch_waveperiod(time)
 
-def test_era5_load_waveheight():
+def test_era5_load_windwaveswellheight():
     pass
 
 def test_era5_load_wavedirection():
@@ -28,5 +28,5 @@ def test_era5_load_waveperiod():
 def test_plot_era5_waveheight():
     filename = "ERA5_reanalysis_significant_height_of_combined_wind_waves_and_swell_2018-01-01_00h.grb2"
     filepath = f"{storage_cfg()}{filename}"
-    data = Era5().load(filepath=filepath, plot="Wave Height 2018-01-01")
+    data = Era5().load(filepath=filepath, plot="Sig. Height of Combined Wind, Wave, and Swell 2018-01-01")
 

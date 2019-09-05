98b9b2fa78cf2ce471b66a763993be79e70f0dec
matt_S <matthew.smith@dal.ca>
2019-09-05 11:28:51 +0000

    removing local hycom files before testing fetch functions

--- kadlu/tests/geospatial/data_sources/test_hycom.py
+++ kadlu/tests/geospatial/data_sources/test_hycom.py
@@ -2,7 +2,10 @@ import pytest
 from datetime import datetime, timedelta
 from kadlu.geospatial.data_sources import hycom
 from kadlu.geospatial.data_sources.hycom import Hycom
+from kadlu.geospatial.data_sources.fetch_util import storage_cfg
+import os
 from os.path import isfile
+from os import remove
 
 # run pytest with '--capture=no' arg to print class function info 
 print(Hycom())
@@ -13,6 +16,12 @@ north =  44.7
 west  = -64.4
 east  = -63.8
 time  = datetime(2015, 1, 1)
+
+# remove fetched files to test fetching
+for f in os.listdir(storage_cfg()):
+    if "hycom" in f:
+        print(f"Removing {f}")
+        remove(f"{storage_cfg()}{f}")
 
 
 def test_fetch_salinity():
@@ -43,11 +52,16 @@ def test_fetch_water_v():
 def test_load_water_v():
     salinity, lat, lon = Hycom().load_water_u(south=south, north=north, west=west, east=east, time=time)
 
-def test_hycom_dt_2_t():
+def test_hycom_dt_2_tslice():
     time = datetime(2015, 1, 1)
-    year_str, daterange = hycom.dt_2_t(time)
-    assert(year_str == "2015")
-    assert(daterange == (0, 0))
+    dateslice = hycom.dt_2_tslice(time)
+    assert(dateslice == 0)
+    time = datetime(2015, 12, 31, 23, 59)
+    dateslice = hycom.dt_2_tslice(time)
+    assert(2859 <= dateslice <= 2860)
+
+    # known issue: hycom time conversion is only accurate within 3 hours
+
     # more test cases for time conversion here
 
 

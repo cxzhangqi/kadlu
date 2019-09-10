6f7b5978157e635a8d319d2e91db2abf960ff2d3
matt_S <matthew.smith@dal.ca>
2019-09-10 18:57:34 +0000

    updated era5 loading

--- kadlu/geospatial/data_sources/era5.py
+++ kadlu/geospatial/data_sources/era5.py
@@ -10,6 +10,8 @@
 """
 
 import cdsapi
+import numpy as np
+import pygrib
 import os
 from datetime import datetime, timedelta
 from kadlu.geospatial.data_sources import fetch_util 
@@ -22,21 +24,38 @@ def fetchname(wavevar, time):
 
 def fetch_era5(wavevar, time):
     fetchfile = f"{storage_cfg()}{fetchname(wavevar, time)}"
-    if os.path.isfile(fetchfile):
-        print(f"File {fetchname(wavevar, time)} exists, skipping retrieval...")
-    else:
-        print(f"Downloading {fetchname(wavevar, time)} from Copernicus Climate Data Store...")
-        c = cdsapi.Client()
-        c.retrieve('reanalysis-era5-single-levels', {
-            'product_type'  : 'reanalysis',
-            'format'        : 'grib',
-            'variable'      : wavevar,
-            'year'          : time.strftime("%Y"),
-            'month'         : time.strftime("%m"),
-            'day'           : time.strftime("%d"),
-            'time'          : time.strftime("%H:%M")
-        }, fetchfile)
+    print(f"Downloading {fetchname(wavevar, time)} from Copernicus Climate Data Store...")
+    c = cdsapi.Client()
+    c.retrieve('reanalysis-era5-single-levels', {
+        'product_type'  : 'reanalysis',
+        'format'        : 'grib',
+        'variable'      : wavevar,
+        'year'          : time.strftime("%Y"),
+        'month'         : time.strftime("%m"),
+        'day'           : time.strftime("%d"),
+        'time'          : time.strftime("%H:%M")
+    }, fetchfile)
     return [fetchfile]
+
+
+def load_era5(wavevar, time, plot):
+    val = np.array([])
+    lat = np.array([])
+    lon = np.array([])
+    fetchfiles = [f"{storage_cfg()}{fetchname(wavevar, time)}"]
+
+    for fname in fetchfiles:
+        if not os.path.isfile(fname):
+            fetch_era5(wavevar, time)
+        grib = pygrib.open(fname)
+        for msg in grib:
+            z, y, x = msg.data()
+            val = np.append(val, z[~z.mask])
+            lat = np.append(lat, y[~z.mask])
+            lon = np.append(lon, x[~z.mask])
+
+    if plot is not False: fetch_util.plot_sample_grib(fetchfiles)
+    return val, lat, lon
 
 
 class Era5():
@@ -46,24 +65,24 @@ class Era5():
         return fetch_era5('mean_wave_direction', time)
     def fetch_waveperiod(self, time=datetime.now()):      
         return fetch_era5('mean_wave_period', time)
+
     def load_windwaveswellheight(self, time=datetime.now(), plot=False):
-        #grbs=pygrib.open(grib)
-        #grb = grbs.select(validDate=target_date,shortNameECMF=wavevar.value)[0]
-        return fetch_util.loadgrib(self.fetch_windwaveswellheight(time), plot)
-    def load_wavedirection(self, time=datetime.now()):
-        return fetch_util.loadgrib(self.fetch_wavedirection(time), plot)
-    def load_waveperiod(self, time=datetime.now()):
-        return fetch_util.loadgrib(self.fetch_waveperiod(time), plot)
+        return load_era5('significant_height_of_combined_wind_waves_and_swell', time, plot)
+    def load_wavedirection(self, time=datetime.now(), plot=False):
+        return load_era5('mean_wave_direction', time, plot)
+    def load_waveperiod(self, time=datetime.now(), plot=False):
+        return load_era5('mean_wave_period', time, plot)
 
     def __str__(self):
         info = "Era5 Global Dataset from Copernicus Climate Datastore"
-        args = "(time=datetime(), plot=False)"
+        args = "(time=datetime())"
         return fetch_util.str_def(self, info, args)
 
 
 """
 print(Era5())
 
+wavevar = 'significant_height_of_combined_wind_waves_and_swell'
 time = datetime(2018, 1, 1)
 fnames = Era5().fetch_windwaveswellheight(time)
 wave, lat, lon = Era5().load_windwaveswellheight(time)
--- kadlu/tests/geospatial/data_sources/test_era5.py
+++ kadlu/tests/geospatial/data_sources/test_era5.py
@@ -1,6 +1,7 @@
 import pytest
 import numpy as np
 from datetime import datetime, timedelta
+from kadlu.geospatial.data_sources import era5
 from kadlu.geospatial.data_sources.era5 import Era5
 from kadlu.geospatial.data_sources.fetch_util import storage_cfg
 from os.path import isfile
@@ -8,29 +9,38 @@ from os.path import isfile
 # time used for testing
 time = datetime(2018, 1, 1, 0, 0, 0, 0)
 
+# disable fetch testing when not necessary to avoid slamming the API with automated requests
+test_fetch = True
+
 def test_era5_fetch_windwaveswellheight():
+    if not test_fetch: return
     filenames = Era5().fetch_windwaveswellheight(time)
     for fname in filenames:
         assert(isfile(fname))
 
 def test_era5_fetch_wavedirection():
+    if not test_fetch: return
     filenames = Era5().fetch_wavedirection(time)
     for fname in filenames:
         assert(isfile(fname))
 
 def test_era5_fetch_waveperiod():
+    if not test_fetch: return
     filenames = Era5().fetch_waveperiod(time)
     for fname in filenames:
         assert(isfile(fname))
 
 def test_era5_load_windwaveswellheight():
-    pass
+    height, lat, lon = Era5().load_windwaveswellheight(time)
+    assert(len(height)==len(lat)==len(lon))
 
 def test_era5_load_wavedirection():
-    pass
+    wave, lat, lon = Era5().load_wavedirection(time)
+    assert(len(wave)==len(lat)==len(lon))
 
 def test_era5_load_waveperiod():
-    pass
+    wave, lat, lon = Era5().load_waveperiod(time)
+    assert(len(wave)==len(lat)==len(lon))
 
 def test_plot_era5_windwaveswellheight():
     filenames = Era5().fetch_windwaveswellheight(time)
@@ -38,6 +48,5 @@ def test_plot_era5_windwaveswellheight():
     for fname in filenames:
         assert(isfile(fname))
     wave, lat, lon = Era5().load_windwaveswellheight(time, plot="Sig. Height of Combined Wind, Wave, and Swell 2018-01-01")
-    #wave, lat, lon = Era5().load(time, plot=False)
     assert(len(wave) == len(lat) == len(lon))
 

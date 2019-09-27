b431a4b86023bab804d599a5ff1e46acef7163e3
matt_S <matthew.smith@dal.ca>
2019-09-27 16:37:56 +0000

    update fetch demo with wwiii module

--- kadlu/__init__.py
+++ kadlu/__init__.py
@@ -2,4 +2,4 @@ from .geospatial.data_sources.chs import Chs as chs
 from .geospatial.data_sources.era5 import Era5 as era5
 from .geospatial.data_sources.rdwps import Rdwps as rdwps
 from .geospatial.data_sources.hycom import Hycom as hycom
-#from .geospatial.data_sources.wwiii import Wiii as wwiii
+from .geospatial.data_sources.wwiii import Wwiii as wwiii

0787d9a14fd3f459d6396a60adbc0dcc86f8c575
matt_S <matthew.smith@dal.ca>
2019-10-23 15:20:36 +0000

    hycom database fetching and loading

--- kadlu/geospatial/data_sources/fetch_util.py
+++ kadlu/geospatial/data_sources/fetch_util.py
@@ -7,17 +7,29 @@ import warnings
 import pygrib
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
+import sqlite3
+from datetime import datetime, timedelta
 
 
-def default_storage(msg):
-    storage_location = (path.abspath(path.dirname(dirname(dirname(dirname(__file__))))) + "/storage/")
-    if not os.path.isdir(storage_location):
-        os.mkdir(storage_location)
-    warnings.warn("%s storage location will be set to %s" % (msg, storage_location))
-    return storage_location
+# database tables
+atmospheric = ['salinity', 'water_temp', 'water_u', 'water_v']
 
 
 def storage_cfg():
+    """ return filepath containing storage configuration string
+
+    first tries to check the config.ini file in kadlu root folder, if there's a 
+    problem defaults to kadlu/storage and issues a warning
+    """
+
+    def default_storage(msg):
+        """ helper function for storage_cfg() """
+        storage_location = (path.abspath(path.dirname(dirname(dirname(dirname(__file__))))) + "/storage/")
+        if not os.path.isdir(storage_location):
+            os.mkdir(storage_location)
+        warnings.warn(f"{msg} storage location will be set to {storage_location}")
+        return storage_location
+
     cfg = configparser.ConfigParser()       # read .ini into dictionary object
     cfg.read(path.join(path.dirname(dirname(dirname(dirname(__file__)))), "config.ini"))
     try:
@@ -34,7 +46,43 @@ def storage_cfg():
     return storage_location
 
 
+def database_cfg():
+    """ connect to sqlite database """
+    path = storage_cfg() + "geospatial.db"
+
+    if not os.path.isfile(path):
+        print(f"creating new database {path}")
+        conn = sqlite3.connect(path)
+        db = conn.cursor()
+        for table in atmospheric:
+            db.execute(f"CREATE TABLE IF NOT EXISTS {table}(val INT, lat REAL, lon REAL, time INT, depth INT, source TEXT)")
+            db.execute(f"CREATE UNIQUE INDEX idx_{table} on {table}(lat, lon, time, depth, source)")
+
+    return sqlite3.connect(path), sqlite3.connect(path).cursor()
+
+
+"""
+db.execute("DROP TABLE salinity")
+"""
+
+
+def dt_2_epoch(dt_arr):
+    """ convert datetimes to epoch hours """
+    t0 = datetime(2000, 1, 1, 0, 0, 0)
+    delta = lambda dt : (dt - t0).total_seconds()/60/60
+    if type(dt_arr) == datetime : return delta(dt_arr)
+    return list(map(delta, dt_arr))
+
+
+def epoch_2_dt(ep_arr):
+    """ convert epoch hours to datetimes """
+    t0 = datetime(2000, 1, 1)
+    return list(map(lambda ep : t0 + timedelta(hours=ep), ep_arr))
+
+
 class Boundary():
+    """ compute intersecting boundaries using the separating axis theorem """
+
     def __init__(self, south, north, west, east, fetchvar=''):
         self.south, self.north, self.west, self.east, self.fetchvar = south, north, west, east, fetchvar
 
@@ -48,7 +96,8 @@ class Boundary():
 
 
 def ll_2_regionstr(south, north, west, east, regions, default=[]):
-    """ convert input bounds to region strings """
+    """ convert input bounds to region strings using Boundary() class and separating axis theorem """
+
     if west > east:  # recursive function call if query intersects antimeridian
         return np.union1d(ll_2_regionstr(south, north, west,  180, regions, default), 
                           ll_2_regionstr(south, north, -180, east, regions, default))
@@ -69,8 +118,6 @@ def str_def(self, info, args):
     strlen = list(map(lambda f : len(f), fcns))
     whitespace = ''.join(map(lambda f : ' ', range(0, np.max(strlen) - np.min(strlen))))
     return f"{info}\n\nClass functions:\n\t" + "\n\t".join(map(lambda f : f"{f}{whitespace[len(f)-np.min(strlen):]}{args}", fcns ))
-
-
 
 
 def plot_sample_grib(gribfiles, title_text="A sample plot"):
--- kadlu/tests/geospatial/data_sources/test_hycom.py
+++ kadlu/tests/geospatial/data_sources/test_hycom.py
@@ -14,32 +14,34 @@ south =  46
 north =  52
 west  = -70
 east  = -56
-start = datetime(2015, 1, 1)
-end   = datetime(2015, 1, 1, 6)
+start = datetime(2000, 1, 2)
+end   = datetime(2000, 1, 2, 4)
 
 # disable automated testing of fetching to avoid slamming the API with
 # requests in the automated development pipeline
-test_fetch = False
+test_fetch = True
 
-# remove fetched files to test fetching
-def unfetch():
-    return 
-    for f in os.listdir(storage_cfg()):
-        if "hycom" in f:
-            print(f"Removing {f}")
-            os.remove(f"{storage_cfg()}{f}")
+# smaller test area for faster tests
+south =  46 
+north =  48
+west  = -60
+east  = -58
 
+def test_hycom_dt_2_tslice():
+    start = datetime(2015, 1, 1)
+    end = datetime(2015, 1, 7)
+    dateslice = hycom.dt_2_tslice(start, end, Hycom().times_dict)
+    assert(dateslice[0] == 0)
+    end = datetime(2015, 12, 31, 23, 59)
+    dateslice = hycom.dt_2_tslice(start, end, Hycom().times_dict)
+    assert(2859 <= dateslice[1] <= 2860)
 
 def test_fetch_salinity():
     if not test_fetch: return
-    unfetch()
-    fnames = Hycom().fetch_salinity(south=south, north=north, west=west, east=east, start=start, end=end)
-    for f in fnames: assert(isfile(f))
+    Hycom().fetch_salinity(south=south, north=north, west=west, east=east, start=start, end=end)
 
 def test_load_salinity():
     val, lat, lon, time, depth = Hycom().load_salinity(south=south, north=north, west=west, east=east, start=start, end=end)
-    # leaving values as 4d array for now
-    pass
     return
     assert(len(val) == len(lat) == len(lon) == len(time))
     assert(sum(lat <= 90) == sum(lat >= -90) == len(lat))
@@ -47,55 +49,72 @@ def test_load_salinity():
 
 def test_fetch_temp():
     if not test_fetch: return
-    unfetch()
-    fnames = Hycom().fetch_temp(south=south, north=north, west=west, east=east, start=start, end=end)
-    for f in fnames: assert(isfile(f))
+    Hycom().fetch_temp(south=south, north=north, west=west, east=east, start=start, end=end)
 
 def test_load_temp():
     val, lat, lon, time, depth = Hycom().load_temp(south=south, north=north, west=west, east=east, start=start, end=end)
-    # leaving values as 4d array for now
-    pass
-    return 
     assert(len(val) == len(lat) == len(lon) == len(time))
     assert(sum(lat <= 90) == sum(lat >= -90) == len(lat))
     assert(sum(lon <= 180) == sum(lon >= -180) == len(lon))
 
 def test_fetch_water_u():
     if not test_fetch: return
-    unfetch()
-    fnames = Hycom().fetch_water_u(south=south, north=north, west=west, east=east, start=start, end=end)
-    for f in fnames: assert(isfile(f))
+    Hycom().fetch_water_u(south=south, north=north, west=west, east=east, start=start, end=end)
 
 def test_load_water_u():
     val, lat, lon, time, depth = Hycom().load_water_u(south=south, north=north, west=west, east=east, start=start, end=end)
-    # leaving values as 4d array for now
-    pass
-    return 
     assert(len(val) == len(lat) == len(lon) == len(time))
     assert(sum(lat <= 90) == sum(lat >= -90) == len(lat))
     assert(sum(lon <= 180) == sum(lon >= -180) == len(lon))
 
 def test_fetch_water_v():
     if not test_fetch: return
-    unfetch()
-    fnames = Hycom().fetch_water_v(south=south, north=north, west=west, east=east, start=start, end=end)
-    for f in fnames: assert(isfile(f))
+    Hycom().fetch_water_v(south=south, north=north, west=west, east=east, start=start, end=end)
 
 def test_load_water_v():
     val, lat, lon, time, depth = Hycom().load_water_u(south=south, north=north, west=west, east=east, start=start, end=end)
-    # leaving values as 4d array for now
-    pass
-    return 
     assert(len(val) == len(lat) == len(lon) == len(time))
     assert(sum(lat <= 90) == sum(lat >= -90) == len(lat))
     assert(sum(lon <= 180) == sum(lon >= -180) == len(lon))
 
-def test_hycom_dt_2_tslice():
-    start = datetime(2015, 1, 1)
-    end = datetime(2015, 1, 7)
-    dateslice = hycom.dt_2_tslice(start, end, Hycom().time)
-    assert(dateslice[0] == 0)
-    end = datetime(2015, 12, 31, 23, 59)
-    dateslice = hycom.dt_2_tslice(start, end, Hycom().time)
-    assert(2859 <= dateslice[1] <= 2860)
+
+""" interactive mode debugging: assert db ordering is correct
+
+    step through fetch_hycom() and put output and grid arrays into memory. 
+    example test input:
+>>>     
+        year = '2012'
+        fetchvar = 'salinity'
+        slices = [
+            (0, 2),         # time: start, end 
+            (0, 3),         # depth: top, bottom
+            (800, 840),     # x grid index: lon min, lon max
+            (900, 1000)     # y grid index: lat min, lat max
+        ]
+        lat, lon = load_grid()
+        dtime = load_times()
+        depth = load_depth()
+
+    run through the output builder loop again. this time, add an assertion to check 
+    that the 4D array was flattened correctly
+>>>     
+        ix = 0  # debug index: assert order is correct
+        for arr in payload.split("\n"):
+            ix_str, row_csv = arr.split(", ", 1)
+            a, b, c = [int(x) for x in ix_str[1:-1].split("][")]
+            # output[a][b][c] = np.array(row_csv.split(", "), dtype=np.int)
+            assert((output[a][b][c] == grid[ix:ix+len(output[a][b][c]), 0]).all())
+            ix += len(output[a][b][c])
+
+"""
+"""
+    import timeit 
+
+    lat, sorted_arr = load_grid()
+    def fcn():
+        return index(np.random.rand()*360 - 180, sorted_arr)
+    
+    timeit.timeit(fcn, number=1000000)
+    
+"""
 

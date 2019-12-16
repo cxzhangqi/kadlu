cd3d63e652e72db4a0571f0e5baff74280e120c2
matt_S <matthew.smith@dal.ca>
2019-12-16 17:31:23 -0400

    fixed bugs in era5 module and updated era5 tests

--- kadlu/geospatial/data_sources/era5.py
+++ kadlu/geospatial/data_sources/era5.py
@@ -69,12 +69,19 @@ def fetch_era5(wavevar, start, end):
         assert(grib.messages == 1)
         msg = grib[1]
         z, y, x = msg.data()
-        x -= 180
+        #x -= 180
 
         # build coordinate grid and insert into db
         src = ['era5' for item in z[~z.mask]]
         t = dt_2_epoch([time for item in z[~z.mask]])
-        grid = list(map(tuple, np.vstack((z[~z.mask].data, y[~z.mask], x[~z.mask], t, src)).T))
+        grid = list(map(tuple, np.vstack((
+                z[~z.mask].data, 
+                y[~z.mask], 
+                ((x[~z.mask] + 180) % 360 ) - 180, 
+                t,
+                src
+            )).T
+        ))
         n1 = db.execute(f"SELECT COUNT(*) FROM {wavevar}").fetchall()[0][0]
         db.executemany(f"INSERT OR IGNORE INTO {wavevar} VALUES (?,?,?,?,?)", grid)
         n2 = db.execute(f"SELECT COUNT(*) FROM {wavevar}").fetchall()[0][0]
@@ -90,37 +97,43 @@ def fetch_era5(wavevar, start, end):
     return 
 
 def load_era5(wavevar, start, end, south, north, west, east):
-    db.execute(' AND '.join([f"SELECT * FROM {wavevar} WHERE lat >= ?",
-                                                            "lat <= ?",
-                                                            "lon >= ?",
-                                                            "lon <= ?",
-                                                           "time >= ?",
-                                                           "time <= ?"]),
-               tuple(map(str, [south, north, west, east, 
-                               dt_2_epoch(start), dt_2_epoch(end)])))
-    val, lat, lon, time, source = np.array(db.fetchall(), dtype=object).T
+    qry = ' AND '.join([f"SELECT * FROM {wavevar} WHERE lat >= ?",
+                                                       "lat <= ?",
+                                                       "lon >= ?",
+                                                       "lon <= ?",
+                                                       "time >= ?",
+                                                       "time <= ?"])
+    db.execute(qry, tuple(map(str, 
+            [south, north, west, east, dt_2_epoch(start)[0], dt_2_epoch(end)[0]]
+        )))
+    slices = np.array(db.fetchall(), dtype=object).T
+    assert len(slices) > 0, "no data found for query"
+    val, lat, lon, time, source = slices
     return val, lat, lon, epoch_2_dt(time)
 
 
 class Era5():
     """ collection of module functions for fetching and loading. abstracted to include a seperate function for each variable """
 
-    def fetch_windwaveswellheight(self, south=-90, north=90, west=-180, east=180, start=datetime.now()-timedelta(hours=24), end=datetime.now()):
+    def fetch_windwaveswellheight(self, start=datetime.now()-timedelta(hours=24), end=datetime.now()):
         return fetch_era5('significant_height_of_combined_wind_waves_and_swell', start, end)
 
-    def fetch_wavedirection(self, south=-90, north=90, west=-180, east=180, start=datetime.now()-timedelta(hours=24), end=datetime.now()):
+    def fetch_wavedirection(self, start=datetime.now()-timedelta(hours=24), end=datetime.now()):
         return fetch_era5('mean_wave_direction', start, end)
 
-    def fetch_waveperiod(self, south=-90, north=90, west=-180, east=180, start=datetime.now()-timedelta(hours=24), end=datetime.now()):
+    def fetch_waveperiod(self, start=datetime.now()-timedelta(hours=24), end=datetime.now()):
         return fetch_era5('mean_wave_period', start, end)
 
-    def load_windwaveswellheight(self, south=-90, north=90, west=-180, east=180, start=datetime.now()-timedelta(hours=24), end=datetime.now()):
+    def load_windwaveswellheight(self, south, north, west, east, 
+            start=datetime.now()-timedelta(hours=24), end=datetime.now()):
         return load_era5('significant_height_of_combined_wind_waves_and_swell', start, end, south, north, west, east)
     
-    def load_wavedirection(self, south=-90, north=90, west=-180, east=180, start=datetime.now()-timedelta(hours=24), end=datetime.now()):
+    def load_wavedirection(self, south, north, west, east,
+            start=datetime.now()-timedelta(hours=24), end=datetime.now()):
         return load_era5('mean_wave_direction', start, end, south, north, west, east)
     
-    def load_waveperiod(self, south=-90, north=90, west=-180, east=180, start=datetime.now()-timedelta(hours=24), end=datetime.now()):
+    def load_waveperiod(self, south, north, west, east,
+            start=datetime.now()-timedelta(hours=24), end=datetime.now()):
         return load_era5('mean_wave_period', start, end, south, north, west, east)
 
     def __str__(self):
@@ -131,3 +144,10 @@ class Era5():
         args = "(south=-90, north=90, west=-180, east=180, start=datetime(), end=datetime())"
         return str_def(self, info, args)
 
+"""
+
+wavevar = 'significant_height_of_combined_wind_waves_and_swell'
+start = datetime(2018, 1, 1, 0, 0, 0, 0)
+end   = datetime(2018, 1, 1, 0, 0, 0, 0)
+
+"""

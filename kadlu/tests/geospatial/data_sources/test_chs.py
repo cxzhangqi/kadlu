91093a5ecb73bb97c316b21dac3cde9e682ee0ca
oliver.kirsebom@dal.ca <oliver.kirsebom@dal.ca>
2019-12-18 17:27:30 -0400

    chs - TST - added assertions for lat,lon, bathy numerical ranges

--- kadlu/tests/geospatial/data_sources/test_chs.py
+++ kadlu/tests/geospatial/data_sources/test_chs.py
@@ -12,6 +12,7 @@
 """
 import pytest
 from kadlu.geospatial.data_sources.chs import Chs
+import numpy as np
 
 source = Chs()
 
@@ -19,6 +20,8 @@ source = Chs()
 south, west = 45.91, -63.81
 north, east = 46.12, -62.92
 
+northumberland_strait_deepest_point = -37
+# http://fishing-app.gpsnauticalcharts.com/i-boating-fishing-web-app/fishing-marine-charts-navigation.html?title=Northumberland+Strait+boating+app#9/46.0018/-63.1677
 
 def test_fetch_bathy():
     source.fetch_bathymetry(south=south, north=north, west=west, east=east)
@@ -27,4 +30,8 @@ def test_load_bathy():
     bathy, lat, lon = source.load_bathymetry(south=south, north=north, west=west, east=east)
     assert (len(bathy) == len(lat) == len(lon))
     assert (len(bathy) > 0)
+    assert np.all(np.logical_and(lat >= south, lat <= north))
+    assert np.all(np.logical_and(lon >= west, lon <= east))
+    assert np.min(bathy) >= northumberland_strait_deepest_point 
+    assert np.max(bathy) <= 2 
 

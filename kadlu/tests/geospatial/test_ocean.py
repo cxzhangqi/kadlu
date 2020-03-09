7f72a140a0948a1e1674172a3a47f20d676a049e
Oliver Kirsebom <oliver.kirsebom@gmail.com>
2020-03-09 18:28:26 -0300

    added more checks to ocean unit tests

--- kadlu/tests/geospatial/test_ocean.py
+++ kadlu/tests/geospatial/test_ocean.py
@@ -75,13 +75,9 @@ def test_uniform_bathy_deriv():
 def test_chs_bathy():
     """ Test that ocean can be initialized with bathymetry data 
         from a CHS file with automatic fetching enabled"""
-    #o = Ocean(default=False, cache=False, fetch=True,
-    #    load_bathymetry='chs', south=43.1, west=-59.8, 
-    #    north=43.8, east=-59.2)
     bound_args = bounds.copy()
     bound_args['south'], bound_args['west'], bound_args['north'], bound_args['east'] = 43.1, -59.8, 43.8, -59.2
     o = Ocean(fetch=8, load_bathymetry='chs', **bound_args)
-    #(bathy,lats,lons) = o.bathy()
     test_lat = [43.4, 43.5]
     test_lon = [-59.6, -59.5]
     bathy = o.bathy(test_lat, test_lon)
@@ -91,18 +87,10 @@ def test_chs_bathy():
     # check that all nodes have meaningful bathymetry values
     assert np.all(bathy < 10000)
     assert np.all(bathy > -15000)
-    # also check boundaries and origin
-    #assert o.origin == LatLon(43.45,-59.5)
-    #assert o.SW == LatLon(43.1,-59.8)
-    #assert o.NE == LatLon(43.8,-59.2)
 
 def test_interp_chs_bathy():
     """ Test that we can interpolate bathymetry data 
         obtained from a CHS file"""
-    #o = Ocean(default=False, cache=False, fetch=True,
-    #    load_bathymetry='chs', south=43.1, west=-59.8, 
-    #    north=43.8, east=-59.2)
-    #b = o.bathy(x=1, y=2)
     o = Ocean(load_bathymetry='chs', 
             south=43.1, west=-59.8, north=43.8, east=-59.2, 
             top=0, bottom=0, start=default_val['start'], end=default_val['end'])
@@ -112,21 +100,14 @@ def test_interp_chs_bathy():
     b = o.bathy(lat=[43.2,43.7], lon=[-59.3, -59.4])
     assert len(b) == 2
 
-def test_hycom_temp_time_interval():
+def test_interp_hycom_temp():
     """ Test that ocean can be initialized with temperature data 
         from HYCOM with automatic fetching enabled and using 
         start/end args.
     """
-    #o = Ocean(cache=False, fetch=True,
-    #    load_temp='hycom', 
-    #    south=43.1, west=-59.8, 
-    #    north=43.8, east=-59.2,
-    #    top=-100, bottom=3000,
-    #    start=datetime(2015,1,1),
-    #    end=datetime(2015,1,2)
-    #    )
     o = Ocean(#fetch=True, #cache=False,
         load_temp='hycom', 
+        load_bathymetry='chs',
         south=43.1, west=-59.8, 
         north=43.8, east=-59.2,
         top=-100, bottom=3000,
@@ -138,12 +119,16 @@ def test_hycom_temp_time_interval():
     depths = [200, 300]
     #(temp,lats,lons,depths) = o.temp()
     temp = o.temp(lats, lons, depths)
-    assert len(temp) > 0 #check that some data was retrieved
     assert  43.1 <= np.min(lats) and np.max(lats) <=  43.8 #check that lats are within limits
     assert -59.8 <= np.min(lons) and np.max(lons) <= -59.2 #check that lons are within limits
-    #assert -3000 <= np.min(depths) and np.max(depths) <= 100 #check that depths are within limits
     assert 3000 >= np.max(depths) and np.min(depths) >= -100 #check that depths are within limits
+    assert len(temp) > 0   #check that some data was retrieved
     assert len(depths) > 1 #check that more than one depth was fetched
+    assert np.all(np.abs(temp - temp.astype(int) > 1e-12)) # check that temperatures are not integers
+    # interpolate temperature at seafloor
+    seafloor_depth = o.bathy(lats, lons)
+    temp = o.temp(lats, lons, seafloor_depth)
+    assert np.all(np.logical_and(temp > -5, temp < 105)) # check sensible temperature at bottom
 
 def test_array_bathy():
     """ Test that ocean can be initialized with bathymetry data 
@@ -152,15 +137,10 @@ def test_array_bathy():
                       [-100., -200.]])
     lats = np.array([44.5, 44.7])
     lons = np.array([-60.1, -59.5])
-    #o = Ocean(default=False, cache=False, fetch=True,
-    #    load_bathymetry=(bathy,lats,lons))
-    
     # note that fetching does nothing when supplying raw array data
     o = Ocean(load_bathymetry=(bathy, lats, lons), fetch=True, **bounds)
-
-    #(b,la,lo) = o.bathy()
-    la = lats #[44.55, 44.65]
-    lo = lons #[-60, -59.75]
+    la = lats
+    lo = lons
     b = o.bathy(lat=lats, lon=lons)
     assert np.all(b == bathy)
     assert np.all(la == lats)
@@ -169,11 +149,3 @@ def test_array_bathy():
     assert res == -100
     res = o.bathy(lat=44.5, lon=-59.8)
     assert pytest.approx(res == -150., abs=1e-6)
-
-
-""" Interactive testing
-    south, west = 44, -59
-    north, east = 46, -57
-    start, end = datetime(2015, 1, 10), datetime(2015, 1, 10, 12)
-    top, bottom = 0, 5000
-"""

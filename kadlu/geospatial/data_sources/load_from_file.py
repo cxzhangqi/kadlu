9df06017f05837e1229c8f838d344ed04214f1b3
matt24smith <matt24smith@gmail.com>
2020-06-25 21:55:50 -0300

    added regional subset querying to raster loading and updated setup.py

--- kadlu/geospatial/data_sources/load_from_file.py
+++ kadlu/geospatial/data_sources/load_from_file.py
@@ -20,31 +20,25 @@ from kadlu.geospatial.data_sources.data_util        import          \
 
 
 def load_raster(filepath, plot=False, cmap=None, **kwargs):
-    """ load data from raster file """
+    """ load data from raster file 
+
+        args:
+            filepath: string
+                complete filepath descriptor of netcdf file to be read
+            plot: boolean
+                if True, a plot will be displayed using the qt5agg backend
+            cmap: matplotlib colormap
+                the full list of available colormaps can be viewed with:
+                print(matplotlib.pyplot.colormaps())
+                if None is supplied, pyplot will default to 
+                matplotlib.pyplot.cm.cividis
+
+        returns:
+            values: numpy 2D array
+            lats:   numpy 1D array
+            lons:   numpy 1D array
     """
-    #var = 'bathymetry'
-    filepath = storage_cfg() + 'gebco_2020_n90.0_s0.0_w-90.0_e0.0.tif'
-    filepath = storage_cfg() + 'test.tif'
-    filepath = storage_cfg() + 'GEBCO_BATHY_2002-01-01_rgb_3600x1800.TIFF'
-    filepath = storage_cfg() + 'BlueMarbleNG_2004-12-01_rgb_3600x1800.TIFF'
 
-    plot=True
-    cmap=None
-    #kwargs=dict(south=-90, west=-180, north=90, east=180, top=0, bottom=50000)
-    kwargs=dict(south=60, west=-, north=61, east=180, top=0, bottom=50000)
-
-    kwargs['north'], kwargs['east'] = 60.4709, 61.26033
-    kwargs['south'], kwargs['west'] = 60.46333,61.15905
-
-    kwargs['north'], kwargs['east'] = 62.4709,  63.26033
-    kwargs['south'], kwargs['west'] = 60.46333, 61.15905
-
-We have two moorings 2019-2020  August 2019 to end September 2020  (yes, some futures in there as well).
-  Mooring HiBioA 2019-2020 … located at:   60 degrees, 28.254 minutes N,  61 degrees, 15.620 W
-  Mooring HiBioC 2019-2020 … located at:   60 degrees, 27.7998 minutes N,  61 degrees, 09.543 W
-
-    """
-    
     # load raster
     Image.MAX_IMAGE_PIXELS = 500000000
     im = Image.open(filepath)
@@ -57,10 +51,13 @@ We have two moorings 2019-2020  August 2019 to end September 2020  (yes, some fu
         meta        = im.tag_v2[42112]  # GdalMetadata
         xml         = ET.fromstring(meta)
         params      = {tag.attrib['name'] : tag.text for tag in xml}
-        logging.info(f'{tree.tag}\nraster coordinate system: {im.tag_v2[34737]}\n{json.dumps(params, indent=2, sort_keys=True)}')
-        lat = np.arange(y, y + (dy * im.size[1]), dy)[ :: -1]
-        #rng_lat = index(kwargs['west'], lat),  index(kwargs['east'], lat)
-        rng_lat = index(kwargs['south'], lat),  index(kwargs['north'], lat)
+        lat = np.arange(y, y + (dy * im.size[1]), dy)[ :: -1] -90
+        rng_lat = (abs(index(kwargs['north'], lat[::-1])-len(lat)), 
+                   abs(index(kwargs['south'], lat[::-1])-len(lat)))
+        lon = np.arange(x, x + (dx * im.size[0]), dx)
+        rng_lon = index(kwargs['east'], lon), index(kwargs['west'], lon)
+        logging.info(f'{xml.tag}\nraster coordinate system: {im.tag_v2[34737]}'
+                     f'\n{json.dumps(params, indent=2, sort_keys=True)}')
 
     # NASA / jet propulsion labs raster format (page 27)
     # https://landsat.usgs.gov/sites/default/files/documents/geotiff_spec.pdf
@@ -68,17 +65,15 @@ We have two moorings 2019-2020  August 2019 to end September 2020  (yes, some fu
         dx,_,_,x,_,dy,_,y,_,_,dz,z,_,_,_,_ = im.tag_v2[34264]  # ModelTransformationTag
         lat = np.arange(y, y + (dy * im.size[1]), dy)
         rng_lat = index(kwargs['south'], -lat),  index(kwargs['north'], -lat)
+        lon = np.arange(x, x + (dx * im.size[0]), dx)
+        rng_lon = index(kwargs['west'], lon), index(kwargs['east'], lon)
 
     else: assert False, 'unknown metadata tag encoding'
-
-    lon = np.arange(x, x + (dx * im.size[0]), dx)
-    rng_lon = index(kwargs['west'], lon), index(kwargs['east'], lon)
-
     assert not (z or dz), '3D rasters not supported yet'
 
     # construct grid and decode pixel values
-    if reduce(np.multiply, (rng_lon[1] - rng_lon[0], rng_lat[1] - rng_lat[0])) > 10000000: logging.info('this could take a few moments...')
-    #grid = np.ndarray(list(map(int, reduce(np.append, (im.size[0],np.array(im.getpixel((0,0))).shape)))))
+    if reduce(np.multiply, (rng_lon[1] - rng_lon[0], rng_lat[1] - rng_lat[0])) > 10000000: 
+        logging.info('this could take a few moments...')
     grid = np.ndarray((len(np.arange(rng_lon[0], rng_lon[1])), len(np.arange(rng_lat[0], rng_lat[1]))))
     for yi in np.arange(rng_lon[0], rng_lon[1]) -rng_lon[0]: 
         grid[yi] = np.array(list(map(im.getpixel, zip(
@@ -87,29 +82,26 @@ We have two moorings 2019-2020  August 2019 to end September 2020  (yes, some fu
         ))))
     mask = grid == float(im.tag_v2[42113])
     val = np.ma.MaskedArray(grid, mask=mask)
-    x1, y1 = np.meshgrid(lon[rng_lon[0]:rng_lon[1]], -1*lat[rng_lat[0]:rng_lat[1]], indexing='ij')
 
+    # plot the data
     if plot:
+        x1, y1 = np.meshgrid(lon[rng_lon[0]:rng_lon[1]], lat[rng_lat[0]:rng_lat[1]], indexing='ij')
         fig = plt.figure()
-        ax = fig.add_subplot(1,1,1, projection='scatter_density')
-        if borderless:
-            ax.set_xticks([]), ax.set_yticks([])
+        if (rng_lon[1]-rng_lon[0]) * (rng_lat[1]-rng_lat[0]) >= 100000:
+            ax = fig.add_subplot(1,1,1, projection='scatter_density')
             plt.axis('scaled')
-            raster = ax.scatter_density(x1, y1, c=val, cmap=cmap, vmin=0, vmax=256)
-            #raster = ax.scatter_density(lon, lat, c=val, cmap=cmap, vmin=0, vmax=256)
-            plt.tight_layout()
-            plt.show()
-            #ax.scatter(x1, y1, c=val)
-            #fig.savefig(storage_cfg()+'figures/test_raster.png', figsize=(200,200), dpi=300)
-        else:
-            ax.set_title(filepath)
             raster = ax.scatter_density(x1, y1, c=val, cmap=cmap)
-            plt.show()
-    
-    return val, y1, x1
+            plt.tight_layout()
+        else:
+            ax = fig.add_subplot(1,1,1)
+            ax.scatter(x1, y1, c=val, cmap=cmap)
+        plt.show()
+
+    #return val, y1, x1
+    return val, lat[rng_lat[0]:rng_lat[1]], lon[rng_lon[0]:rng_lon[1]]
 
 
-def load_netcdf(filename, var=None, **kwargs):
+def load_netcdf(filename, var=None, plot=False, cmap=None, **kwargs):
     """ read environmental data from netcdf and output to gridded numpy array
 
         args:
@@ -118,6 +110,13 @@ def load_netcdf(filename, var=None, **kwargs):
             var: string (optional)
                 the netcdf attribute to be read as the values.
                 by default, a guess will be made based on the file metadata
+            plot: boolean
+                if True, a plot will be displayed using the qt5agg backend
+            cmap: matplotlib colormap
+                the full list of available colormaps can be viewed with:
+                print(matplotlib.pyplot.colormaps())
+                if None is supplied, pyplot will default to 
+                matplotlib.pyplot.cm.cividis
 
         returns:
             values: numpy 2D array
@@ -143,6 +142,20 @@ def load_netcdf(filename, var=None, **kwargs):
     val = ncfile[ var ][:].data[rng_lat[0]:rng_lat[1], rng_lon[0]:rng_lon[1]]
     lat = ncfile['lat'][:].data[rng_lat[0]:rng_lat[1]]
     lon = ncfile['lon'][:].data[rng_lon[0]:rng_lon[1]]
+
+    # plot the data
+    if plot:
+        x1, y1 = np.meshgrid(lon[rng_lon[0]:rng_lon[1]], lat[rng_lat[0]:rng_lat[1]], indexing='ij')
+        fig = plt.figure()
+        if (rng_lon[1]-rng_lon[0]) * (rng_lat[1]-rng_lat[0]) >= 100000:
+            ax = fig.add_subplot(1,1,1, projection='scatter_density')
+            plt.axis('scaled')
+            raster = ax.scatter_density(x1, y1, c=val, cmap=cmap)
+            plt.tight_layout()
+        else:
+            ax = fig.add_subplot(1,1,1)
+            ax.scatter(x1, y1, c=val, cmap=cmap)
+        plt.show()
 
     return val, lat, lon
 

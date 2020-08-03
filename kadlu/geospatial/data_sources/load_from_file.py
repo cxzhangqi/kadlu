867b16019a8f8eab5d2b0b426feeb1210a1e9bc8
matt_S <matthew.smith@dal.ca>
2020-08-03 18:40:22 -0300

    CI test

--- kadlu/geospatial/data_sources/load_from_file.py
+++ kadlu/geospatial/data_sources/load_from_file.py
@@ -6,7 +6,7 @@ import json
 from datetime import datetime
 
 import matplotlib
-'Qt5Agg' in matplotlib.rcsetup.all_backends and matplotlib.use('Qt5Agg')
+if 'Qt5Agg' in matplotlib.rcsetup.all_backends: matplotlib.use('Qt5Agg')
 import mpl_scatter_density
 import matplotlib.pyplot as plt
 import netCDF4
